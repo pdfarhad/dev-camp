e8f5b3d95a99a9b444da47a6b89b6cd20f5bb982
farhad naeem <pdcsedu@gmail.com>
2021-08-15 13:12:47 +0600

    middleware and database connector added

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const dotenv = require("dotenv");
-
-
-const bootcamps = require('./routes/bootcamps');
+const logger = require('./middleware/logger')
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const colors = require('colors');
 
 dotenv.config({
     path: './config/config.env'
 });
 
+connectDB();
+
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+// Logger middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+// app.use(logger);
 
 app.use('/api/v1/bootcamps', bootcamps)
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+// 
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    server.close(() => process.exit(1));
+})
